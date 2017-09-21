c1c20cc213f091ed73695eaef3e1bf446bfae0f6
Justin Kim <jjkim928@gmail.com>
2017-09-20 21:25:08 -0400

    created for loop to instantiate 3 new teachers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,14 +6,20 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         List<Student> students = new ArrayList<>();
 
         for (int i = 0; i < 15; i++) {
-
             students.add(new Student("Student" + String.valueOf(i), "Kim", i));
         }
 
+        List<Teacher> teachers = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            teachers.add(new Teacher("Teacher" + String.valueOf(i), "Lee", i));
+        }
+
         System.out.println(students);
+        System.out.println(teachers);
     }
 }
