f502abd88669cb6d31e1cc3b4b8e15b1583fab01
Justin Kim <jjkim928@gmail.com>
2017-09-20 21:31:24 -0400

    created 3 separate sets of students

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -21,5 +20,28 @@ public class Main {
 
         System.out.println(students);
         System.out.println(teachers);
+
+        //Map<Teacher, Set<Student>> assignments = new HashMap<>(); hmm, gonna try creating the set of students first
+
+        // Group1
+        Set<Student> group1 = new HashSet<>();
+        for (int i = 0; i < 5; i++) {
+            group1.add(students.get(i));
+        }
+        System.out.println(group1);
+
+        // Group2
+        Set<Student> group2 = new HashSet<>();
+        for (int i = 5; i < 10; i++) {
+            group2.add(students.get(i));
+        }
+        System.out.println(group2);
+
+        // Group3
+        Set<Student> group3 = new HashSet<>();
+        for (int i = 10; i < 15; i++) {
+            group3.add(students.get(i));
+        }
+        System.out.println(group3);
     }
 }
