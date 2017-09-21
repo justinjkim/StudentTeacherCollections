b643eb249fd03bce10c78346dde73e27ade11290
Justin Kim <jjkim928@gmail.com>
2017-09-20 21:37:05 -0400

    starting to look ugly...but assigned sets of students to each teacher

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,27 +21,36 @@ public class Main {
         System.out.println(students);
         System.out.println(teachers);
 
-        //Map<Teacher, Set<Student>> assignments = new HashMap<>(); hmm, gonna try creating the set of students first
+
 
         // Group1
         Set<Student> group1 = new HashSet<>();
         for (int i = 0; i < 5; i++) {
             group1.add(students.get(i));
         }
-        System.out.println(group1);
+        //System.out.println(group1);
 
         // Group2
         Set<Student> group2 = new HashSet<>();
         for (int i = 5; i < 10; i++) {
             group2.add(students.get(i));
         }
-        System.out.println(group2);
+        //System.out.println(group2);
 
         // Group3
         Set<Student> group3 = new HashSet<>();
         for (int i = 10; i < 15; i++) {
             group3.add(students.get(i));
         }
-        System.out.println(group3);
+        //System.out.println(group3);
+
+
+        Map<Teacher, Set> assignments = new HashMap<>();
+        assignments.put(teachers.get(0), group1);
+        assignments.put(teachers.get(1), group2);
+        assignments.put(teachers.get(2), group3);
+
+
+        System.out.println(assignments);
     }
 }
