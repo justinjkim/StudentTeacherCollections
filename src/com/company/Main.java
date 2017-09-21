e16c741e2b2583a08d06782d128aa5eb1aa3aaad
Justin Kim <jjkim928@gmail.com>
2017-09-20 20:39:58 -0400

    created initial array of students

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        List<Student> students = new ArrayList<>();
+
+        
     }
 }
